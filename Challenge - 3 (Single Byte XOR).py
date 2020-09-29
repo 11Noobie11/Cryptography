879f5dca2b4b216d26e441db34744bd52876e6e8
Rahul Sreenivasan <71322795+11Noobie11@users.noreply.github.com>
2020-09-29 14:22:56 +0530

    Add files via upload

---
+++ Challenge - 3 (Single Byte XOR).py
@@ -0,0 +1,8 @@
+#Single Byte XOR
+
+from binascii import unhexlify
+hx = unhexlify('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
+for key in range(256):
+    decoded = ''.join(chr(b ^ key) for b in hx) #Doin xor on each byte of the decoded bytes
+    if decoded.isprintable():
+        print(key, decoded)
